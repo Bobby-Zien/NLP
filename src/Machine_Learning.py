3649db9ca656da4188d603f02a9527f0f596c2e9
yjjw <yjjw97@gmail.com>
2022-04-26 13:53:36 -0400

    Integrated Path and Vector Feature Selection to ML Models

--- src/Machine_Learning.py
+++ src/Machine_Learning.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -8,8 +9,10 @@ from sklearn.naive_bayes import CategoricalNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
+
+feature_selection = ""  # set by argv
 training_features = []
 training_labels = []
 testing_features = []
@@ -28,6 +31,8 @@ label_count = 1
 label_reverse_map = dict()
 stem_map = dict()
 stem_count = 1
+path_map = dict()
+path_count = 1
 # the longest sentence length in the training file
 max_sentence_len = 0
 
@@ -57,6 +62,7 @@ def constructMap(train_file_name, test_file_name):
     global BIO_count
     global pos_count
     global stem_count
+    global path_count
     training_lines = extractFile(train_file_name)
     testing_lines = extractFile(test_file_name)
     print(f'the length of the training lines is {len(training_lines)}')
@@ -77,9 +83,9 @@ def constructMap(train_file_name, test_file_name):
             if total_lines[i][0] not in word_map:
                 word_map[total_lines[i][0]] = word_count
                 word_count += 1
-            # POS, stem, BIO
+            # POS, stem, BIO, Path
             for element in total_lines[i]:
-                if "POS=" in element:
+                if "POS=" in element in element:
                     pos = element.split("=")[1]
                     if pos not in pos_map:
                         pos_map[pos] = pos_count
@@ -94,12 +100,18 @@ def constructMap(train_file_name, test_file_name):
                     if bio not in BIO_map:
                         BIO_map[bio] = BIO_count
                         BIO_count += 1
+                if "path=" in element:
+                    path = element.split("=")[1]
+                    if path not in path_map:
+                        path_map[path] = path_count
+                        path_count += 1
 
 
 def encodeFile(file_type):
     if file_type == "training":
         global training_features
         global training_labels
+        global max_sentence_len
         training_features = []
         training_labels = []
         for line in training_lines:
@@ -111,7 +123,7 @@ def encodeFile(file_type):
                     if i == len(line) - 1:
                         training_labels.append(label_map[line[i]])
                     else:
-                        if "POS=" in line[i]:
+                        if "POS=" in line[i] or "POS2=" in line[i]:
                             pos = line[i].split("=")[1]
                             curr_line.append(pos_map[pos])
                         elif "stem=" in line[i]:
@@ -120,12 +132,22 @@ def encodeFile(file_type):
                         elif "BIO=" in line[i]:
                             bio = line[i].split("=")[1]
                             curr_line.append(BIO_map[bio])
-                        elif "word=" in line[i]:
+                        elif "sim=" in line[i]:
+                            sim = (float(line[i].split("=")[1]) + 1) * 10000
+                            curr_line.append(str(sim))
+                        elif "top5" in line[i] or "top3" in line[i] or "top1" in line[i]:
+                            curr_line.append(line[i].split("=")[1])
+                        elif "word=" in line[i] or "word2=" in line[i]:
                             word = line[i].split("=")[1]
                             curr_line.append(word_map[word])
+                        elif "path=" in line[i]:
+                            path = line[i].split("=")[1]
+                            curr_line.append(path_map[path])
 
                 training_features.append(curr_line)
 
+        max_sentence_len = max(map(len, training_features))
+        print(f'the longest sentence length in the training file is {max_sentence_len}')
         print(f'the length of training features is {len(training_features)}')
         print(f'the length of training labels is {len(training_labels)}')
 
@@ -138,7 +160,7 @@ def encodeFile(file_type):
                 for i in range(len(line)):
                     if i == 0:
                         curr_line.append(word_map[line[i]])
-                    if "POS=" in line[i]:
+                    if "POS=" in line[i] or "POS2=" in line[i]:
                         pos = line[i].split("=")[1]
                         curr_line.append(pos_map[pos])
                     elif "stem=" in line[i]:
@@ -147,27 +169,43 @@ def encodeFile(file_type):
                     elif "BIO=" in line[i]:
                         bio = line[i].split("=")[1]
                         curr_line.append(BIO_map[bio])
-                    elif "word=" in line[i]:
+                    elif "sim=" in line[i]:
+                        sim = (float(line[i].split("=")[1]) + 1) * 10000
+                        curr_line.append(str(sim))
+                    elif "top5" in line[i] or "top3" in line[i] or "top1" in line[i]:
+                        curr_line.append(line[i].split("=")[1])
+                    elif "word=" in line[i] or "word2" in line[i]:
                         word = line[i].split("=")[1]
                         curr_line.append(word_map[word])
+                    elif "path=" in line[i]:
+                        path = line[i].split("=")[1]
+                        curr_line.append(path_map[path])
             testing_features.append(curr_line)
+
         print(f'the length of testing features is {len(testing_features)}')
     else:
         sys.exit('invalid file type')
 
 
-# read the training lines and split it to features and labels
+# scale each value to the range(0, 1) based on the min-max in the column
+def scale(arr):
+    for j in range(max_sentence_len):
+        curr_col_max = max(arr[:, j])
+        for i in range(arr.shape[0]):
+            arr[i, j] = arr[i, j] / float(curr_col_max)
+    return arr
+
+# padding and scaling, and then fitting the model
 # fit the model using features and labels
 def fitModel(model_type):
-    global max_sentence_len
     encodeFile("training")
-    max_sentence_len = max(map(len, training_features))
-    print(f'the longest sentence length in the training file is {max_sentence_len}')
     features = np.array([np.pad(sentence, (0, max_sentence_len - len(sentence)), 'constant') for sentence in training_features])
     print(f'the shape of training features is {features.shape}')
-    labels = np.array(training_labels, dtype=object)
+    features = features.astype('float')
+    # features = scale(features)
+    labels = np.array(training_labels, dtype=object).astype('float')
     print(f'the shape of training labels is {labels.shape}')
-    labels = labels.astype('int')
+
     if model_type == "naive_bayes":
         nb.fit(features, labels)
     elif model_type == "logistic_regression":
@@ -182,6 +220,8 @@ def predict(model_type):
         [np.pad(sentence, (0, max_sentence_len - len(sentence)), 'constant') for sentence in testing_features])
 
     print(f'the shape of testing feature is {features.shape}')
+    features = features.astype('float')
+    # features = scale(features)
     if model_type == "naive_bayes":
         label_pred = nb.predict(features)
     elif model_type == "logistic_regression":
@@ -194,12 +234,29 @@ def predict(model_type):
 
 
 def write_output(model_type, testing_lines, label_pred):
-    if model_type == "naive_bayes":
-        out = open("outputs/nb_output.txt", 'w')
-    elif model_type == "logistic_regression":
-        out = open("outputs/reg_output.txt", 'w')
-    elif model_type == "SVM":
-        out = open("outputs/svm_output.txt", 'w')
+    if feature_selection == "stem":
+        if model_type == "naive_bayes":
+            out = open("../outputs/nb_stem.txt", 'w')
+        elif model_type == "logistic_regression":
+            out = open("../outputs/reg_stem.txt", 'w')
+        elif model_type == "SVM":
+            out = open("../outputs/svm_stem.txt", 'w')
+    elif feature_selection == "path":
+        if model_type == "naive_bayes":
+            out = open("../outputs/nb_path.txt", 'w')
+        elif model_type == "logistic_regression":
+            out = open("../outputs/reg_path.txt", 'w')
+        elif model_type == "SVM":
+            out = open("../outputs/svm_path.txt", 'w')
+    elif feature_selection == "vector":
+        if model_type == "naive_bayes":
+            out = open("../outputs/nb_vector.txt", 'w')
+        elif model_type == "logistic_regression":
+            out = open("../outputs/reg_vector.txt", 'w')
+        elif model_type == "SVM":
+            out = open("../outputs/svm_vector.txt", 'w')
+
+
 
     for i in range(len(testing_lines)):
         if not testing_lines[i]:
@@ -213,13 +270,23 @@ def write_output(model_type, testing_lines, label_pred):
 def main(args):
     training_file = args[1]
     testing_file = args[2]
+    global feature_selection
+    if "stem" in training_file and "stem" in testing_file:
+        feature_selection += "stem"
+    elif "path" in training_file and "path" in testing_file:
+        feature_selection += "path"
+    elif "vec" in training_file and "vec" in testing_file:
+        feature_selection += "vector"
+    else:
+        sys.exit("invalid arguments provided, double checked please")
+
     constructMap(training_file, testing_file)
-    fitModel("naive_bayes")
-    predict("naive_bayes")
+    # fitModel("naive_bayes")
+    # predict("naive_bayes")
+    fitModel("SVM")
+    predict("SVM")
     # fitModel("logistic_regression")
     # predict("logistic_regression")
-    # fitModel("SVM")
-    # predict("SVM")
 
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
